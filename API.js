9cbb3d3f4353c302cebdd235c914ea807528b5ef
MAD-2 <mmuradiabdullah88@gmail.com>
2022-03-15 01:02:08 +0300

    >>

--- API.js
+++ API.js
@@ -59,9 +59,9 @@ fetch(catApi)
             <div class="row g-0 border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
               <div class="col p-4 d-flex flex-column position-static">
                 <strong class="d-inline-block mb-2 text-primary"></strong>
-                <h3 class="mb-0">${news.title}</h3>
+                <h3 class="mb-0 text-dark">${news.title}</h3>
                 <div class="mb-1 text-muted">${news.author}</div>
-                <p class="card-text mb-auto">${news.description}</p>
+                <p class="card-text mb-auto text-dark">${news.description}</p>
                 <a href="${news.url}" class="stretched-link">Continue reading</a>
               </div>
               <div class="col-auto d-none d-lg-block">
